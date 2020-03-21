6b23f8ba62958483f2c0be1286eb08e3681ba476
Mike Ludemann <mike-ludemann@hotmail.de>
2020-03-21 14:40:36 +0100

    Setup complete - vue-load-more-content

---
+++ src/components/load-more-content.vue
@@ -0,0 +1,68 @@
+<template lang="html">
+
+	<section class="load-more-content">
+		<div :class="classname" v-for="data in datas" :key="data">{{data.content}}</div>
+		<div :id="id">Load More</div>
+	</section>
+
+</template>
+
+<script>
+import $ from 'jquery';
+
+	export default {
+		name: 'LoadMoreContent',
+		props: {
+			id: {
+				type: String
+			},
+			classname: {
+				type: String
+			},
+			datas: {
+				type: Array
+			},
+			count: {
+				type: Number
+			}
+		},
+		mounted () {
+			let cln = "." + this.classname;
+      const idn = "#" + this.id;
+      let cnt = this.count;
+			$(document).ready(function() {
+				$(cln).slice(0, cnt).show();
+				$(cln).slice(cnt, $(cln).length).hide();
+				if ($(cln + ":hidden").length !== 0) {
+					$(idn).show();
+				}
+				$(idn).on('click', function(e) {
+					e.preventDefault();
+					$(cln + ":hidden").slice(0, cnt).slideDown();
+					if ($(cln + ":hidden").length === 0) {
+						$(idn).fadeOut('slow');
+					}
+				});
+			});
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+
+		}
+}
+
+
+</script>
+
+<style scoped lang="scss">
+	.load-more-content {
+
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoadMoreContent id="loadMore" classname="list" count="2" :datas='[{id: 1, content: "1"}, {id: 2, content: "2"}, {id: 3, content: "3"}, {id: 4, content: "4"}, {id: 5, content: "5"}]'/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import LoadMoreContent from '@/components/load-more-content.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    LoadMoreContent
+  }
+}
+</script>
